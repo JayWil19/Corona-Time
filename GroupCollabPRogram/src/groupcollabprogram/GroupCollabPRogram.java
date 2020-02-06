0c71e17a32ba117c9355ebb42cb199838584c7cc
JayWil19 <56897171+JayWil19@users.noreply.github.com>
2020-02-06 13:34:08 -0500

    Sample Program for the project

---
+++ GroupCollabPRogram/src/groupcollabprogram/GroupCollabPRogram.java
@@ -0,0 +1,8 @@
+package groupcollabprogram;
+/*@author jtw22*/
+public class GroupCollabPRogram {
+    public static void main(String[] args) {
+        System.out.println("Jacob Wilson");
+    }
+    
+}
