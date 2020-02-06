9d546e45bf5480967aa1a2ce5284db3fed95b2fb
Cjparke11@gmail.com <>
2020-02-06 14:07:04 -0500

    Cory Parke - new static void, added middle name

--- GroupCollabPRogram/src/groupcollabprogram/GroupCollabPRogram.java
+++ GroupCollabPRogram/src/groupcollabprogram/GroupCollabPRogram.java
@@ -2,7 +2,12 @@ package groupcollabprogram;
 /*@author jtw22*/
 public class GroupCollabPRogram {
     public static void main(String[] args) {
+        middle();
         System.out.println("Jacob Wilson");
     }
+    public static void middle(){
+        System.out.println("Thomas");
+    }
+    
     
 }
