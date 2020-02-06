fca4ca4f15598a6e396b3016268bfac7bc512e19
cjslicer51 <56699992+cjslicer51@users.noreply.github.com>
2020-02-06 13:59:04 -0500

    Corey John new random numbers

--- GroupCollabPRogram/src/groupcollabprogram/GroupCollabPRogram.java
+++ GroupCollabPRogram/src/groupcollabprogram/GroupCollabPRogram.java
@@ -3,6 +3,10 @@ package groupcollabprogram;
 public class GroupCollabPRogram {
     public static void main(String[] args) {
         System.out.println("Jacob Wilson");
+        yeet();
+        
     }
-    
+    public static void yeet(){
+        System.out.println("35,6,67,88,99876,5,433333323456,7890,98765,43,4567,89,0,9876,543");
+    }
 }
